f2e2f973e5591efca1612ebd7a1e9cf436afa112
Shubham Kumar <shubham908@gmail.com>
2025-02-04 19:01:38 +0530

    Formatted the file

--- main.tf
+++ main.tf
@@ -1,28 +1,28 @@
 resource "aws_security_group" "tf_sg_web_server" {
-  name = "tf-sg-web-server"
+  name        = "tf-sg-web-server"
   description = "Allow HTTP traffic to the web server"
 
   ingress {
     description = "HTTP ingress"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "my_vm" {
-  ami           = var.ami
-  instance_type = var.instance_type
+  ami                    = var.ami
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tf_sg_web_server.id]
-  user_data = <<EOF
+  user_data              = <<EOF
 #!/bin/bash
 yum update -y
 yum install -y httpd
